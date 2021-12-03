85d25227eb0d4ec94d8e269a4eef26d031a224b0
kan5016@naver.com <kan5016@naver.com>
2021-12-03 19:40:42 +0900

    [update] mainFragment onbackpressed dialog

--- LazyBirdProject/app/src/main/java/com/limit/lazybird/ui/MainFragment.kt
+++ LazyBirdProject/app/src/main/java/com/limit/lazybird/ui/MainFragment.kt
@@ -1,16 +1,22 @@
 package com.limit.lazybird.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.limit.lazybird.R
 import com.limit.lazybird.databinding.FragmentMainBinding
+import com.limit.lazybird.models.DialogInfo
 import com.limit.lazybird.ui.calendar.CalendarFragment
 import com.limit.lazybird.ui.earlybird.EarlyBirdFragment
 import com.limit.lazybird.ui.exhibition.ExhibitionFragment
 import com.limit.lazybird.ui.mybird.MyBirdFragment
+import com.limit.lazybird.ui.onboarding.CustomDialogFragment
 import com.limit.lazybird.ui.search.SearchFragment
 import com.limit.lazybird.util.replaceChildFragment
 
@@ -24,6 +30,11 @@ class MainFragment: Fragment(R.layout.fragment_main) {
     private val currentChildFragment: LiveData<Int> get() = _currentChildFragment
 
     private lateinit var binding : FragmentMainBinding
+    private val parentActivity: MainActivity by lazy {
+        activity as MainActivity
+    }
+
+    lateinit var callback: OnBackPressedCallback
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -71,10 +82,54 @@ class MainFragment: Fragment(R.layout.fragment_main) {
             }
             return@setOnItemSelectedListener false
         }
+
+        setFragmentResultListener(CustomDialogFragment.TAG) { _, bundle ->
+            when (bundle.getString("resultCode")) {
+                CustomDialogFragment.RESULT_OK -> {
+                    parentActivity.supportFragmentManager.popBackStack()
+                }
+            }
+        }
+
     }
 
     private fun updateChildFragment(page: Int) {
         if(_currentChildFragment.value != page)
             _currentChildFragment.value = page // 페이지가 동일할 땐 화면을 변경하지 않는다.
+    }
+
+    private fun showDialog() {
+        val dialogInfo = DialogInfo(
+            title = resources.getString(R.string.main_exit_title),
+            message = "",
+            positiveBtnTitle = resources.getString(R.string.main_exit_yes),
+            negativeBtnTitle = resources.getString(R.string.main_exit_no)
+        )
+        CustomDialogFragment().apply {
+            // dialog 정보 보내주기
+            arguments = bundleOf().apply {
+                putParcelable(CustomDialogFragment.DIALOG_INFO, dialogInfo)
+            }
+        }.show(
+            parentActivity.supportFragmentManager,
+            CustomDialogFragment.TAG
+        )
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if(parentActivity.supportFragmentManager.backStackEntryCount == 0){
+                    showDialog()
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callback.remove()
     }
 }
