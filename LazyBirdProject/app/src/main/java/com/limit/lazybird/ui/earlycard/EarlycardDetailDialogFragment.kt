13abc62404815824a4033b39d0f547e9e43dfe36
kan5016@naver.com <kan5016@naver.com>
2021-12-14 04:24:02 +0900

    [update] screenshot in earlycard fragment

--- LazyBirdProject/app/src/main/java/com/limit/lazybird/ui/earlycard/EarlycardDetailDialogFragment.kt
+++ LazyBirdProject/app/src/main/java/com/limit/lazybird/ui/earlycard/EarlycardDetailDialogFragment.kt
@@ -1,13 +1,22 @@
 package com.limit.lazybird.ui.earlycard
 
+import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.DialogFragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
@@ -16,6 +25,9 @@ import com.bumptech.glide.request.RequestOptions
 import com.limit.lazybird.R
 import com.limit.lazybird.databinding.DialogEarlycardDetailBinding
 import com.limit.lazybird.models.EarlycardInfo
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
 
 /************* EarlycardDetailDialogFragment ***************
  * 메인화면(?? 탭) >> 얼리카드 화면 >> 얼리카드 크게보기 (DialogFragment)
@@ -28,7 +40,14 @@ class EarlycardDetailDialogFragment : DialogFragment() {
         const val EARLYCARD_INFO = "EARLYCARD_INFO"
     }
 
+    private val REQUEST_EXTERNAL_STORAGE = 1
+    private val PERMISSION_STORAGE = arrayOf(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
     private lateinit var binding: DialogEarlycardDetailBinding
+    private var parentActivity: Activity? = null
     private var alertDialog: AlertDialog? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -55,7 +74,11 @@ class EarlycardDetailDialogFragment : DialogFragment() {
             }
             earlycardDetailSaveBtn.setOnClickListener {
                 // 스크린샷 버튼 클릭
-                clickTicketSave()
+                if(Build.VERSION.SDK_INT >= 30){
+                    clickTicketSave()
+                } else {
+                    clickTicketSave()
+                }
             }
         }
 
@@ -75,11 +98,49 @@ class EarlycardDetailDialogFragment : DialogFragment() {
 
     private fun clickTicketSave() {
         // 티켓이미지 저장 버튼 클릭
-        Toast.makeText(requireContext(), "저장이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+        verifyStoragePermission()
+        takeScreenShot()
+        Toast.makeText(requireContext(), "스크린샷 저장이 완료되었습니다.", Toast.LENGTH_SHORT).show()
     }
 
     private fun clickClose() {
         // 취소 버튼 클릭 시
         dismiss()
+    }
+
+    fun verifyStoragePermission() {
+        val permission =
+            ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                parentActivity!!,
+                PERMISSION_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    private fun takeScreenShot() {
+        val view = alertDialog!!.window!!.decorView.rootView
+        view.isDrawingCacheEnabled = true
+        val bitmap = Bitmap.createBitmap(view.drawingCache)
+        view.isDrawingCacheEnabled = false
+        val filePath =
+            Environment.getExternalStorageDirectory().path + "/Download/" + Calendar.getInstance().time.time.toString() + ".jpg"
+        val fileScreenShot = File(filePath)
+
+        try {
+            val fileOutputStream = FileOutputStream(fileScreenShot)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+            fileOutputStream.flush()
+            fileOutputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentActivity = activity
     }
 }
